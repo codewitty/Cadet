e2ae1f19624efa2684d35e2b22644e43cfd9bbd5
codewitty <gomesjoshua@gmail.com>
2018-11-12 14:23:07 -0800

    Memcpy finally works. Removed the test for a null src from while loop and made a seperate if stmt to check for null

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,13 +6,13 @@
 /*   By: jogomes <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/05 11:58:37 by jogomes           #+#    #+#             */
-/*   Updated: 2018/11/12 13:49:57 by jogomes          ###   ########.fr       */
+/*   Updated: 2018/11/12 14:20:03 by jogomes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_memcpy(void *restrict dst, const void *restrict src, size_t len)
+char	*ft_memcpy(void *dst, const void *restrict src, size_t len)
 {
 	size_t		i;
 	const char	*src1;
@@ -21,7 +21,9 @@ char	*ft_memcpy(void *restrict dst, const void *restrict src, size_t len)
 	src1 = src;
 	dst1 = dst;
 	i = 0;
-	while (src1[i] && i < len)
+	if (dst == (NULL) || src == (NULL))
+		return (NULL);
+	while (i < len)
 	{
 		dst1[i] = src1[i];
 		i++;
