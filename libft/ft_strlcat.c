0375b72f576d2937d7feed2a70cbd837b8f0741a
codewitty <35624908+codewitty@users.noreply.github.com>
2018-11-27 21:39:45 -0800

    new functions added

---
+++ libft/ft_strlcat.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jogomes <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/05 11:58:37 by jogomes           #+#    #+#             */
+/*   Updated: 2018/11/12 14:20:03 by jogomes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char		*ft_strlcat(char *s1, const char *s2, size_t size)
+{
+	size_t	j;
+	size_t	k;
+
+	j = 0;
+	k = 0;
+	while (dst[j] && j < size)
+		j++;
+	while ((src[k]) && ((j + k + 1) < size))
+	{
+		dst[j + k] = src[k];
+		k++;
+	}
+	if (j != size)
+		dst[j + k] = '\0';
+	return (j + ft_strlen(src));
+
+}
