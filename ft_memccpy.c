bf0910a08a114f299acd9c010d9981f6a7fd7f47
codewitty <gomesjoshua@gmail.com>
2018-12-07 15:56:56 -0800

    new

---
+++ ft_memccpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jogomes <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/05 11:58:37 by jogomes           #+#    #+#             */
+/*   Updated: 2018/12/02 14:27:57 by jogomes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memccpy(void *dst, const void *src, int c, size_t len)
+{
+	size_t		i;
+	char		*src2;
+	char		*dst2;
+
+	dst2 = (char *)dst;
+	src2 = (char *)src;
+	i = 0;
+	while (i < len)
+	{
+		dst2[i] = src2[i];
+		if ((unsigned char)src2[i] == (unsigned char)c)
+			return ((char *)dst + i + 1);
+		i++;
+	}
+	return (NULL);
+}
---
+++ ft_memchr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jogomes <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/05 11:58:37 by jogomes           #+#    #+#             */
+/*   Updated: 2018/12/02 13:43:18 by jogomes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *src, int c, size_t n)
+{
+	char	*str;
+	size_t	i;
+
+	i = 0;
+	str = (char *)src;
+	while (i < n)
+	{
+		if ((unsigned char)str[i] == (unsigned char)c)
+			return ((char *)src + i);
+		i++;
+	}
+	return (NULL);
+}
