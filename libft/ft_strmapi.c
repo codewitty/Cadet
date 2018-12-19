76f899e95a80998d92600df2d6f62ce1f901338b
codewitty <gomesjoshua@gmail.com>
2018-12-19 13:39:02 -0800

    new fns

--- libft/ft_strmapi.c
+++ libft/ft_strmapi.c
@@ -1,16 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strmap.c                                        :+:      :+:    :+:   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jogomes <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/19 12:53:19 by jogomes           #+#    #+#             */
-/*   Updated: 2018/12/19 13:36:20 by jogomes          ###   ########.fr       */
+/*   Updated: 2018/12/19 13:38:39 by jogomes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strmap(char const *s, char (*f)(char))
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
 	char	*p;
 	int		i;
@@ -22,7 +22,7 @@ char	*ft_strmap(char const *s, char (*f)(char))
 	p = (char *)malloc(sizeof(char) * (i + 1));
 	while (s[i])
 	{
-		p[i] = f(&s[i]);
+		p[i] = f(i, &s[i]);
 		i++;
 	}
 	p[i] = '\0';
