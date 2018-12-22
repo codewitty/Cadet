e866081e498348bbd731cbf191809f4dca504e85
codewitty <35624908+codewitty@users.noreply.github.com>
2018-12-21 19:02:33 -0800

    new func

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -14,17 +14,21 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char *p;
+	char	*p;
+	int	i;
+	int	j;
 
-	p = (char *)malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
-	p = s1;
-	while (*p)
-		p++;
-	while (*s2)
+	p = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+
+	while (s1[i])
 	{
-		*p = *s2;
-		p++;
-		s2++;
+		p[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		p[i+j] = s2[j];
+		j++;
 	}
 	*p = '\n';
 	return (p);
