dd15adc7c8c43bb94d9aaa034ebdbd4ff6db03ea
codewitty <35624908+codewitty@users.noreply.github.com>
2019-01-05 23:30:35 -0800

    new fns

--- libft/ft_lstnew.c
+++ libft/ft_lstnew.c
@@ -11,9 +11,3 @@
 /* ************************************************************************** */
 
 int		ft_strequ(char const *s1, char const *s2)
-{
-	if (ft_strcmp(s1, s2) == 0)
-		return (1);
-	else
-		return (0);
-}
