828c7bf76ac5ef74a26bd6e57dd42f641c65593a
codewitty <35624908+codewitty@users.noreply.github.com>
2019-01-30 22:14:13 -0800

    changes

--- libft/ft_strsplit.c
+++ libft/ft_strsplit.c
@@ -12,54 +12,14 @@
 
 #include "libft.h"
 
-/*This function will allocate memory and return a 'fresh' table of strings (all
- * terminated by a '\0', the table as well) as a result of the splitting of
- * the given string by the given character c. If memory allocation fails at
- * any point the function will return NULL. And example of this function is
- * ft_strsplit("*hello*fellow***students*", '*'). This should return a table
- * that splits the string int ["hello", "fellow", "students"]. This function
- * must be done in three parts. We will start at the ft_strsplit function at
- * the bottom. We put ft_strsplit at the end of our code because in order to 
- * use other functions we make specifically for it we have to make sure we have
- * them made before we get to our actual function.*/
-
-/*The first function we have is a word counting function. We make this because
- * we will need to figure out how many strings we will be placing into our 
- * table that's to be returned from the ft_strsplit function. We want this
- * for when we allocate memory for the table.*/
 static int			ft_cntwrd(char const *s, char c)
 {
-	/*We start by creating two variables. The first is an unsigned int i which
-	 * we will use to move through the string that was given to ft_strsplit.
-	 * We use unsigned in the event we get a ridiculously long string. We also
-	 * make a counter variable we will be using to count the words in the
-	 * string and then we will return it. NOTE: This function returns a static
-	 * int. A static variable as a way of getting around not using global
-	 * variables. A static variable keeps its value between invocations. We 
-	 * use this for cases where a function needs to keep some state between
-	 * invocations. We set both of our variables to 0 and begin our function.*/
 	unsigned int	i;
 	int				cntr;
 
 	i = 0;
 	cntr = 0;
-	/*We go into our while loop and have it loop so long as we have not reached
-	 * the end of the given string. Inside our loop we have another while loop
-	 * that is saying so long as the index position we are in has the same
-	 * character as our c parameter then we want to move past it. This is the
-	 * same c character that the ft_strsplit function will be splitting the
-	 * string by. So we want to skip over that character until we find our
-	 * first real word. If, when our while loop has finished passing over
-	 * c characters, we have not reached the end of the given string then we
-	 * want to increase our word counter. We know that we have something that
-	 * is not the character held by variable c and we want to split that from
-	 * the string. We then go to another while loop that will pass over all of
-	 * the characters in our word until we find another instance of the 
-	 * character held by variable c. If we find one and we have not reached the
-	 * end of our string we begin the loop again and continue until we have
-	 * reached the end, counting all of the words along the way. We then
-	 * return the cntr variable which will tell us how many words exist in our
-	 * string.*/
+	
 	while (s[i])
 	{
 		while (s[i] == c)
