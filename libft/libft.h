4fc47678b0c9a1d36a2a26427e8a40d9f45933a7
Joshua Gomes <gomesjoshua@gmail.com>
2019-12-12 22:04:25 -0800

    changes

--- libft/libft.h
+++ libft/libft.h
@@ -51,7 +51,7 @@ char	*ft_strnstr(const char *s1, const char *s2, size_t n);
 void	*ft_memalloc(size_t size);
 void	ft_memdel(void **ap);
 char	*ft_strnew(size_t size);
-void	*ft_strdel(char **as);
+void	ft_strdel(char **as);
 void	ft_strclr(char *s);
 void	ft_striter(char *s, void (*f)(char *));
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
