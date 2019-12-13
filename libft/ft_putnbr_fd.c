658a59f7c372b810874cb705f4605694a76c854b
Joshua Gomes <gomesjoshua@gmail.com>
2019-12-12 22:08:58 -0800

    nchanges

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -12,6 +12,26 @@
 
 #include "libft.h" 
 
-void	ft_putnbr_fd(char c, int fd)
+void	ft_putnbr_fd(int n, int fd)
 {
-	unsigned int i;
+		if (fd < 0)
+		return ;
+	if (n == -2147483648)
+	{
+		ft_putstr_fd("-2147483648", fd);
+	}
+	else if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		ft_putnbr_fd(-n, fd);
+	}
+	else if (n >= 10)
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putchar_fd(n % 10 + '0', fd);
+	}
+	else
+	{
+		ft_putchar_fd(n + '0', fd);
+	}
+}
--- libft/ft_putstr.c
+++ libft/ft_putstr.c
@@ -3,23 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putstr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jogomes <marvctrn@42.fr>                     +#+  +:+       +#+        */
+/*   By: jogomes <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/22 14:19:24 by jogomes           #+#    #+#             */
 /*   Updated: 2018/11/12 13:52:00 by jogomes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ctrnclude "lctrbft.h"
+#include "libft.h"
 
 void		ft_putstr(char const *s)
 {
-	size_t	ctr;
+	size_t	i;
 
-	ctr = 0;
-	while (s[ctr])
+	i = 0;
+	while (s[i])
 	{
-		ft_putchar(s[ctr]);
-		ctr++;
+		ft_putchar(s[i]);
+		i++;
 	}
 }
