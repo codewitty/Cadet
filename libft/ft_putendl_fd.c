6b5bf7a84d4e50f27544e777c2f68551abfd0c2d
Joshua Gomes <gomesjoshua@gmail.com>
2019-12-12 22:10:26 -0800

    nchanges

--- libft/ft_putendl_fd.c
+++ libft/ft_putendl_fd.c
@@ -12,6 +12,10 @@
 
 #include "libft.h" 
 
-void	ft_putendl_fd(char c, int fd)
+void	ft_putendl_fd(char const *s, int fd)
 {
-	unsigned int i;
+	if (!s || fd < 0)
+		return ;
+	ft_putstr_fd(s, fd);
+	ft_putchar_fd('\n', fd);
+}
